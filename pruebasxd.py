813e9a6fab3cf8a0fb43ee7add393a1d0feefaa2
jazgamarra <j44min@gmail.com>
2023-05-23 12:33:34 -0400

    funciona el cambio de clima :p

--- pruebasxd.py
+++ pruebasxd.py
@@ -1,22 +1,3 @@
-
-class Juego: 
-    ''' Almacena las mecanicas del juego '''
-
-    def __init__ (self, poblacion, clima ):
-        self.turno_juego = 0
-        self.poblacion = poblacion
-        self.clima = clima 
-
-    def gestionar_turno (self): 
-        self.turno += 1
-        self.clima_gestionar_clima()
-    
-    def mostrar_scores (): 
-        return None
-
-    def calcular_poblacion(): 
-        return None 
-
 class Clima: 
     ''' Muchas de las mecanicas del juego dependen del clima en el que se encuentra. ''' 
     def __init__ (self):
@@ -28,11 +9,11 @@ class Clima:
         ''' Modificar el clima dependiendo de si corresponda al turno. Gestiona el contador de clima y las llamadas al verificador de clima. '''
         
         self.contador_clima +=1 
-        self.estacion_actual = self.verificar_estacion()
+        self.estacion_actual = self.cambiar_estacion()
 
-    def verificar_estacion(self):
+    def cambiar_estacion(self):
         ''' Se encarga de modificar las estaciones dependiendo de la estacion actual'''
-
+        
         def modificar_estacion(i, nueva_estacion, estacion_actual):
             if self.contador_clima == i:
                 self.contador_clima = 0
@@ -41,5 +22,15 @@ class Clima:
 
         return modificar_estacion(3, 'invierno', 'verano') if self.estacion_actual == 'verano' else modificar_estacion(2, 'verano', 'invierno')
         
-        
+    
+
+clima = Clima()
+
+
+for i in range(10):  # Ejecutamos el ciclo 10 veces para mostrar los cambios de estación
+    print("Estación actual:", clima.estacion_actual, clima.contador_clima)
+    clima.gestionar_clima(i)
+    
+
+    
 
