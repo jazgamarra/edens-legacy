ee26705066a12657c5843629e0f04d938583b907
jazgamarra <j44min@gmail.com>
2023-06-13 16:30:22 -0400

    funciona maomeno la interfaz maomeno modularizada

--- Vista/widgets.py
+++ Vista/widgets.py
@@ -1,23 +1,35 @@
 from tkinter import * 
 from PIL import ImageTk, Image 
 from Vista.paleta_de_colores import * 
+from colorama import Fore
+from Modelo.clima_estacional import ClimaEstacional 
+from Controlador.mecanicas_juego import Juego 
+from Modelo.mapa import Mapa
+from Vista.entrada_y_salida import EntradaSalida
 
 class Interfaz: 
+    def __init__ (self): 
+        self.clima = ClimaEstacional() 
+        self.sociedad = None
+        self.mapa = Mapa()
+        self.juego = None
+        self.vista = None
+        self.root = Tk() 
+
+        self.inicializar_interfaz()
+
+    def inicializar_interfaz(self): 
+        ''' Configuraciones generales la interfaz de tkinter. '''
+        self.root.geometry('1200x800')
+        self.root.configure(bg=crema)
+        self.root.title('welcome to eden\'s legacy! ') 
 
 
-    def presentacion_del_juego(): 
-        def __init__ (self, nombre, coordenadas, codigo): 
-            clima = ClimaEstacional() 
-            sociedad = None
-            mapa = Mapa()
-            juego = Juego(sociedad, clima, mapa)
-            vista = EntradaSalida(juego)
-
-
-
+    def presentacion_del_juego(self): 
+       
         # Cambiar el color del fondo 
-        root.configure(bg=verde)
-        frame = LabelFrame(root, pady=200, bg=verde, border=0)
+        self.root.configure(bg=verde)
+        frame = LabelFrame(self.root, pady=200, bg=verde, border=0)
 
         # Texto 
         Label(frame, text='''Sumérgete en cada turno en un cautivador escenario visual, donde tendrás la\ntarea de elegir sabiamente tu próximo destino y la acción a emprender. \nDeliberadamente decidirás entre explorar con valentía o colonizar con astucia \ndiversos ecosistemas, persiguiendo así la preciada adquisición de recursos vitales.''',  font=('Arial', 14), bg=verde, fg=crema).pack() 
@@ -30,19 +42,21 @@ class Interfaz:
         button_frame.pack()
         frame.pack()
 
-    def elegir_raza(): 
+    def elegir_raza(self): 
         def definir_raza(raza): 
-            juego.sociedad = vista.definir_raza(raza) 
+            self.juego = Juego(self.sociedad, self.clima, self.mapa)
+            self.vista = EntradaSalida(self.juego)
+            self.juego.sociedad = self.vista.definir_raza(raza) 
 
         def cambiar_ventana(raza): 
             definir_raza(raza)
             frame.pack_forget()
-            presentacion_del_juego()
+            self.presentacion_del_juego()
 
 
-        root.configure(bg=azul_marino)
+        self.root.configure(bg=azul_marino)
 
-        frame = LabelFrame(root, padx=10, pady=50, bg=azul_marino, border=0)
+        frame = LabelFrame(self.root, padx=10, pady=50, bg=azul_marino, border=0)
         titulo = Label(frame, text='Elegir la raza', bg=azul_marino, font=('Roboto Cn', 22), fg=crema).pack()
         subtitulo = Label(frame, text='''En un desolado y exánime universo, donde solo una raza ha prevalecido, \ndespierta tu espíritu aventurero para explorar y colonizar un nuevo y \nmisterioso mundo. Selecciona sabiamente tu raza.''',  font=('Arial', 14), bg=azul_marino, pady=20, fg=crema).pack()
         frame.pack()
@@ -57,15 +71,15 @@ class Interfaz:
         button_frame.pack() 
 
 
-    def pantalla_de_inicio(root): 
+    def pantalla_de_inicio(self): 
         def cambiar_ventana(): 
             logo_frame.pack_forget()
             button.pack_forget()
-            elegir_raza()
+            self.elegir_raza()
 
         # Logo 
         img = ImageTk.PhotoImage(Image.open('static/logo2.png').resize((600, 300))) 
-        logo_frame = LabelFrame(root, pady=150, bg=crema, border=0)
+        logo_frame = LabelFrame(self.root, pady=150, bg=crema, border=0)
         label = Label(logo_frame, text='Eden\'s Legacy', font=('Roboto Cn', 36))
         img_container = Label(logo_frame, image=img, bg=crema)
 
--- app_tinker.py
+++ app_tinker.py
@@ -1,28 +1,8 @@
-from tkinter import * 
-from PIL import ImageTk, Image 
-from Vista.paleta_de_colores import * 
-from Modelo.clima_estacional import ClimaEstacional 
-from Modelo.sociedad import Sociedad 
-from Modelo.raza import Duende
-from Controlador.mecanicas_juego import Juego 
-from Modelo.mapa import Mapa
-from Vista.entrada_y_salida import EntradaSalida
-
-# Configuraciones del tkinter 
-root = Tk()
-root.title('welcome to eden\'s legacy! ') 
-root.geometry('1200x800')
-root.configure(bg=crema)
-
-# Configuraciones del juego 
-clima = ClimaEstacional() 
-sociedad = None
-mapa = Mapa()
-juego = Juego(sociedad, clima, mapa)
-vista = EntradaSalida(juego)
+from Vista.widgets import * 
+ 
+i = Interfaz() 
 
 
+i.pantalla_de_inicio()
 
-pantalla_de_inicio(root)
-
-root.mainloop()
+i.root.mainloop()
